8156c4a06a120091b9f9d871913e5f2193ef2775
Robert Cofield <robertgcofield@gmail.com>
2016-01-19 13:37:10 -0600

    replace unlabeled full 7 phase diagram with the full spec version

---
+++ scripts/general_segment_solving_vertical.m
@@ -0,0 +1,144 @@
+%%
+% clear all; close all; clc
+% 
+% j = [1 0 -1 0 -1 0 1];
+% t = [0 1 2 3 4 5 6 7];
+% jpp = mkpp(t,j);
+% app = fnint(jpp, 0);
+% vpp = fnint(app, 0);
+% spp = fnint(vpp, 0);
+% 
+% tt = linspace(t(1), t(end), 500);
+% ss = fnval(spp, tt);
+% vv = fnval(vpp, tt);
+% aa = fnval(app, tt);
+% jj = fnval(jpp, tt);
+% 
+% f = figure
+% subplot(4,1,1)
+% plot(tt,ss, 'linewidth', 6)
+% grid on
+% whitebg('k')
+% ylabel('Pos', 'FontSize', 16, 'FontWeight', 'bold')
+% title('7 Phase Profile')
+% subplot(4,1,2)
+% plot(tt,vv, 'linewidth', 6)
+% grid on
+% whitebg('k')
+% ylabel('Speed', 'FontSize', 16, 'FontWeight', 'bold')
+% subplot(4,1,3)
+% plot(tt,aa, 'linewidth', 6)
+% grid on
+% ylabel('Accel', 'FontSize', 16, 'FontWeight', 'bold')
+% whitebg('k')
+% subplot(4,1,4)
+% plot(tt,jj, 'linewidth', 6)
+% grid on
+% whitebg('k')
+% ylabel('Jerk', 'FontSize', 16, 'FontWeight', 'bold')
+% xlabel('Time', 'FontSize', 16, 'FontWeight', 'bold')
+
+%%
+clear all; close all; clc
+
+j = [1 0 -1 0 -1 0 1];
+t = [0 .5 1.5 2.5 4 5 6.9+[0 1]];
+jpp = mkpp(t,j);
+app = fnint(jpp, .5);
+vpp = fnint(app, 1);
+spp = fnint(vpp, 0);
+
+tt = linspace(t(1), t(end), 500);
+ss = fnval(spp, tt);
+vv = fnval(vpp, tt);
+aa = fnval(app, tt);
+jj = fnval(jpp, tt);
+
+t_ = [t(1), t(end)];
+
+f = figure;
+p1 = subplot(4,1,1);
+hold on
+h = plot(tt,ss, 'linewidth', 6);
+plot(t_, [15 15], '--c', 'linewidth', 3)
+text(1, 14, 'L', 'Color', 'c', 'FontSize', 16, 'VerticalAlignment', 'top')
+grid on
+whitebg('k')
+uistack(h, 'top')
+ylabel('Distance', 'FontSize', 16, 'FontWeight', 'bold')
+% xlabel('Time', 'FontSize', 16, 'FontWeight', 'bold')
+
+
+p2 = subplot(4,1,2);
+hold on
+h = plot(tt,vv, 'linewidth', 6);
+plot([0 2], [1 1], '--c', 'linewidth', 3)
+text(2,1, 'v_i','Color', 'c', 'FontSize', 16, 'VerticalAlignment', 'top', 'FontWeight', 'bold')
+plot([6 8], [0 0], '--c', 'linewidth', 3)
+text(6,0, 'v_f ','Color', 'c', 'FontSize', 16, 'VerticalAlignment', 'bottom', 'HorizontalAlignment', 'right', 'FontWeight', 'bold')
+plot(t_, [2.9 2.9], '--c', 'linewidth', 3)
+text(8, 2.5, 'v_m ', 'Color', 'c', 'FontSize', 16, 'FontWeight', 'bold', 'HorizontalAlignment', 'right')
+uistack(h, 'top')
+grid on
+whitebg('k')
+ylabel('Speed', 'FontSize', 16, 'FontWeight', 'bold')
+% xlabel('Time', 'FontSize', 16, 'FontWeight', 'bold')
+
+
+p3 = subplot(4,1,3);
+hold on
+h = plot(tt,aa, 'linewidth', 6);
+plot([0 3], [1 1], '--c', 'linewidth', 3)
+text(3, 1, 'a_p^+', 'Color', 'c', 'FontSize', 16, 'VerticalAlignment', 'top', 'FontWeight', 'bold')
+plot([0 4], [1.5 1.5], '--r', 'linewidth', 3)
+text(4, 1.5, 'a_m^+', 'Color', 'r', 'FontSize', 16, 'VerticalAlignment', 'top', 'FontWeight', 'bold')
+plot([2.5 8], [-1.5, -1.5], '--r', 'linewidth', 3)
+text(2.5, -1.5, 'a_m^- ', 'Color', 'r', 'FontSize', 16, 'VerticalAlignment', 'bottom', 'FontWeight', 'bold', 'HorizontalAlignment', 'right')
+plot([3.5 8], [-1 -1], '--c', 'linewidth', 3)
+text(3.5, -1, 'a_p^-', 'Color', 'c', 'FontSize', 16, 'VerticalAlignment', 'bottom', 'FontWeight', 'bold', 'HorizontalAlignment', 'right')
+grid on
+ylabel('Accel', 'FontSize', 16, 'FontWeight', 'bold')
+uistack(h, 'top')
+whitebg('k')
+% xlabel('Time', 'FontSize', 16, 'FontWeight', 'bold')
+
+
+p4 = subplot(4,1,4);
+hold on
+h = plot(tt,jj, 'linewidth', 6);
+plot([0 2.5], [1 1], '--c', 'linewidth', 3)
+text(2.5, 1, 'j_p^+', 'Color', 'c', 'FontSize', 16, 'VerticalAlignment', 'top', 'FontWeight', 'bold')
+plot([0 2.5], [1.5 1.5], '--r', 'linewidth', 3)
+text(2.5, 1.5, 'j_m^+', 'Color', 'r', 'FontSize', 16, 'VerticalAlignment', 'top', 'FontWeight', 'bold')
+plot([0 2.5], [-1 -1], '--c', 'linewidth', 3)
+text(2.5, -1, ' -j_p^+', 'Color', 'c', 'FontSize', 16, 'VerticalAlignment', 'bottom', 'FontWeight', 'bold')
+plot([0 2.5], [-1.5 -1.5], '--r', 'linewidth', 3)
+text(2.5, -1.5, '-j_m^+', 'Color', 'r', 'FontSize', 16, 'VerticalAlignment', 'bottom', 'FontWeight', 'bold')
+plot([4 8], [-1.5, -1.5], '--r', 'linewidth', 3)
+text(4, -1.5, 'j_m^- ', 'Color', 'r', 'FontSize', 16, 'VerticalAlignment', 'bottom', 'FontWeight', 'bold', 'HorizontalAlignment', 'right')
+plot([4 8], [-1 -1], '--c', 'linewidth', 3)
+text(4, -1, 'j_p^- ', 'Color', 'c', 'FontSize', 16, 'VerticalAlignment', 'bottom', 'FontWeight', 'bold', 'HorizontalAlignment', 'right')
+plot([4 8], [1.5, 1.5], '--r', 'linewidth', 3)
+text(4, 1.5, '-j_m^- ', 'Color', 'r', 'FontSize', 16, 'VerticalAlignment', 'top', 'FontWeight', 'bold', 'HorizontalAlignment', 'right')
+plot([4 8], [1 1], '--c', 'linewidth', 3)
+text(4, 1, '-j_p^- ', 'Color', 'c', 'FontSize', 16, 'VerticalAlignment', 'top', 'FontWeight', 'bold', 'HorizontalAlignment', 'right')
+text(0.1, -0.5, '\Delta t_1')
+text(0.9, -0.5, '\Delta t_2')
+text(1.9, 0.5, '\Delta t_3')
+text(3, 0.5, '\Delta t_4')
+text(4.3, 0.5, '\Delta t_5')
+text(5.9, -0.5, '\Delta t_6')
+text(7.3, -0.5, '\Delta t_7')
+grid on
+whitebg('k')
+uistack(h, 'top')
+ylabel('Jerk', 'FontSize', 16, 'FontWeight', 'bold')
+xlabel('Time', 'FontSize', 16, 'FontWeight', 'bold')
+
+p1.Position(2) = .8;
+p2.Position(2) = .6;
+p3.Position(2) = .35;
+p3.Position(4) = .2;
+p4.Position(2) = .05;
+p4.Position(4) = .25;
+
